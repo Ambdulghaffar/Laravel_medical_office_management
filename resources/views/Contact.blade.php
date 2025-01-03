8d5dd946184897dfc1d845066aa7cc429114ffd2
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-01-03 16:31:37 +0100

    Add changements

--- resources/views/Contact.blade.php
+++ resources/views/Contact.blade.php
@@ -1,117 +1,5 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-
-    <title>Laravel</title>
-
-
-    {{-- 
-        php artisan config:clear
-        php artisan cache:clear
-        php artisan view:clear 
-        --}}
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <link rel="icon" href={{ asset('medcare-master/img/favicon.png') }} type="image/png">
-
-    <title>Medcare Medical</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ asset('medcare-master/css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('medcare-master/css/themify-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('medcare-master/css/flaticon.css') }}">
-    <link rel="stylesheet" href="{{ asset('medcare-master/vendors/fontawesome/css/all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('medcare-master/vendors/owl-carousel/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('medcare-master/vendors/animate-css/animate.css') }}">
-    <!-- main css -->
-    <link rel="stylesheet" href="{{ asset('medcare-master/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('medcare-master/css/responsive.css') }}">
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="{{ asset('medcare-master/js/jquery-2.2.4.min.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/popper.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/stellar.js') }}"></script>
-    <script src="{{ asset('medcare-master/vendors/owl-carousel/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/jquery.ajaxchimp.min.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/waypoints.min.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/mail-script.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/contact.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/jquery.form.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/jquery.validate.min.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/theme.js') }}"></script>
-
- 
-
-
-
-</head>
-
-<body>
-
-
-    <!--================Header Menu Area =================-->
-    <header class="header_area">
-        <div class="top_menu row m0">
-            <div class="container">
-                <div class="float-left">
-                    <a class="dn_btn" href="mailto:medical@example.com"><i class="ti-email"></i>medical@example.com</a>
-                    <span class="dn_btn"> <i class="ti-location-pin"></i>Find our Location</span>
-                </div>
-                <div class="float-right">
-                    <ul class="list header_social">
-                        <li><a href="#"><i class="ti-facebook"></i></a></li>
-                        <li><a href="#"><i class="ti-twitter-alt"></i></a></li>
-                        <li><a href="#"><i class="ti-linkedin"></i></a></li>
-                        <li><a href="#"><i class="ti-skype"></i></a></li>
-                        <li><a href="#"><i class="ti-vimeo-alt"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="main_menu">
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="container">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <a class="navbar-brand logo_h" href="index.html"><img src="img/logo.png" alt=""></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
-                        <ul class="nav navbar-nav menu_nav ml-auto">
-                            <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-                            <li class="nav-item"><a class="nav-link" href="about-us.html">About</a></li>
-                            <li class="nav-item"><a class="nav-link" href="department.html">Department</a></li>
-                            <li class="nav-item"><a class="nav-link" href="doctors.html">Doctors</a></li>
-                            <li class="nav-item submenu dropdown">
-                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
-                                    aria-haspopup="true" aria-expanded="false">Blog</a>
-                                <ul class="dropdown-menu">
-                                    <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a>
-                                    </li>
-                                    <li class="nav-item"><a class="nav-link" href="element.html">element</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </header>
+@extends('layouts.nav_menu')
+@section('navbar')
     <!--================Header Menu Area =================-->
 
 
@@ -126,8 +14,8 @@
                         <p>Belding years moveth earth green behold wherein</p>
                     </div>
                     <div class="page_link">
-                        <a href="index.html">Home</a>
-                        <a href="contact.html">Contact</a>
+                        <a href="{{ route('/') }}">Acceuil</a>
+                        <a href="{{ route('contact') }}">Contact</a>
                     </div>
                 </div>
             </div>
@@ -420,11 +308,4 @@
     <!--================End Contact Success and Error message Area =================-->
 
 
-
-
-
-
-
-</body>
-
-</html>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,106 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.nav_menu')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-
-    <title>Laravel</title>
-
-
-    {{-- 
-    php artisan config:clear
-    php artisan cache:clear
-    php artisan view:clear 
-    --}}
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <link rel="icon" href={{ asset('medcare-master/img/favicon.png') }} type="image/png">
-
-    <title>Medcare Medical</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ asset('medcare-master/css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('medcare-master/css/themify-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('medcare-master/css/flaticon.css') }}">
-    <link rel="stylesheet" href="{{ asset('medcare-master/vendors/fontawesome/css/all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('medcare-master/vendors/owl-carousel/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('medcare-master/vendors/animate-css/animate.css') }}">
-    <!-- main css -->
-    <link rel="stylesheet" href="{{ asset('medcare-master/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('medcare-master/css/responsive.css') }}">
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="{{ asset('medcare-master/js/jquery-2.2.4.min.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/popper.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/stellar.js') }}"></script>
-    <script src="{{ asset('medcare-master/vendors/owl-carousel/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/jquery.ajaxchimp.min.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/waypoints.min.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/mail-script.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/contact.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/jquery.form.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/jquery.validate.min.js') }}"></script>
-    <script src="{{ asset('medcare-master/js/theme.js') }}"></script>
-
-
-
-</head>
-
-<body>
-
-
-    <!--================Header Menu Area =================-->
-    <header class="header_area">
-        <div class="top_menu row m0">
-            <div class="container">
-                <div class="float-left">
-                    <a class="dn_btn" href="mailto:medical@example.com"><i
-                            class="bi bi-envelope"></i>medical@example.com</a>
-                    <span class="dn_btn"> <i class="bi bi-geo-alt"></i>Find our Location</span>
-                </div>
-                <div class="float-right">
-                    <ul class="list header_social">
-                        <li><a href="#"><i class="bi bi-facebook"></i></a></li>
-                        <li><a href="#"><i class="bi bi-twitter-x"></i></a></li>
-                        <li><a href="#"><i class="bi bi-linkedin"></i></a></li>
-                        <li><a href="#"><i class="bi bi-skype"></i></a></li>
-                        <li><a href="#"><i class="bi bi-vimeo"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="main_menu navbar-fixed-top">
-            <nav class="navbar navbar-expand-lg navbar-light ">
-                <div class="container">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <a class="navbar-brand logo_h" href="index.html"><img
-                            src="{{ asset('medcare-master/img/logo.png') }}" alt=""></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
-                        <ul class="nav navbar-nav menu_nav ml-auto">
-                            <li class="nav-item"><a class="nav-link" href="#"></i>Acceuil</a></li>
-                            <li class="nav-item"><a class="nav-link" href="doctors.html">Inscription</a></li>
-                            <li class="nav-item"><a class="nav-link" href="department.html">Se connecter</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ route('contact') }}">Contact</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </header>
+@section('navbar')
     <!--================Header Menu Area =================-->
 
     <!--================Home Banner Area =================-->
@@ -639,6 +539,4 @@
 
 
 
-</body>
-
-</html>
+@endsection
