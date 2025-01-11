0f04c5fca061954f4983b065e39cae47028db2b0
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-01-11 02:11:07 +0100

    modifications made

--- resources/views/Contact.blade.php
+++ resources/views/Contact.blade.php
@@ -72,7 +72,9 @@
 
             <div class="row">
                 <div class="col-12">
-                    <h2 class="contact-title">Get in Touch</h2>
+                    <h2 class="contact-title">
+                        Entrons en contact
+                    </h2>
                 </div>
                 <div class="col-lg-8">
                     <form class="form-contact contact_form" action="contact_process.php" method="post" id="contactForm"
@@ -81,30 +83,30 @@
                             <div class="col-12">
                                 <div class="form-group">
                                     <textarea class="form-control w-100" name="message" id="message" cols="30" rows="9"
-                                        placeholder="Enter Message"></textarea>
+                                        placeholder="Saisir un message"></textarea>
                                 </div>
                             </div>
                             <div class="col-sm-6">
                                 <div class="form-group">
                                     <input class="form-control" name="name" id="name" type="text"
-                                        placeholder="Enter your name">
+                                        placeholder="Entrez votre nom complet">
                                 </div>
                             </div>
                             <div class="col-sm-6">
                                 <div class="form-group">
                                     <input class="form-control" name="email" id="email" type="email"
-                                        placeholder="Enter email address">
+                                        placeholder="Entrez l'adresse e-mail">
                                 </div>
                             </div>
                             <div class="col-12">
                                 <div class="form-group">
                                     <input class="form-control" name="subject" id="subject" type="text"
-                                        placeholder="Enter Subject">
+                                        placeholder="Entrez le sujet">
                                 </div>
                             </div>
                         </div>
                         <div class="form-group mt-3">
-                            <button type="submit" class="button button-contactForm">Send Message</button>
+                            <button type="submit" class="button button-contactForm">Envoyer un message</button>
                         </div>
                     </form>
 
@@ -115,22 +117,22 @@
                     <div class="media contact-info">
                         <span class="contact-info__icon"><i class="ti-home"></i></span>
                         <div class="media-body">
-                            <h3>Buttonwood, California.</h3>
-                            <p>Rosemead, CA 91770</p>
+                            <h3>Mhannech, Tétouan.</h3>
+                            <p>55, rue Mly Abdellah</p>
                         </div>
                     </div>
                     <div class="media contact-info">
                         <span class="contact-info__icon"><i class="ti-tablet"></i></span>
                         <div class="media-body">
                             <h3><a href="tel:454545654">(+212) 644-239452</a></h3>
-                            <p>Mon to Fri 9am to 6pm</p>
+                            <p>Du lundi au vendredi de 9h à 15h</p>
                         </div>
                     </div>
                     <div class="media contact-info">
                         <span class="contact-info__icon"><i class="ti-email"></i></span>
                         <div class="media-body">
-                            <h3><a href="mailto:support@colorlib.com">support@colorlib.com</a></h3>
-                            <p>Send us your query anytime!</p>
+                            <h3><a href="mailto:support@colorlib.com">medical@example.com</a></h3>
+                            <p>Envoyez-nous votre requête à tout moment !</p>
                         </div>
                     </div>
                 </div>
