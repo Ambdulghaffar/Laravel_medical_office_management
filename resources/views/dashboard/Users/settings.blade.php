88b47bf39848194af454d8c0249c52b10af5a906
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-05-23 16:05:19 +0100

    settings page commit

--- resources/views/dashboard/Users/settings.blade.php
+++ resources/views/dashboard/Users/settings.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.dashboard_layout')
 
 @section('dashboard')
-
-<p>bonjour</p>
-
-@endsection
+    <div class="col-12">
+        <div class="bg-light rounded h-100 p-4">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb bg-light pt-3 rounded fs-5">
+                    <li class="breadcrumb-item"><a href="{{ route('dashboard') }}"><i class="bi bi-house"></i> Dashboard</a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">Paramètres</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+        @endsection
